18c31e2f19b65b9fd8c81c1db51c3a6dfb58728e
vidhan <vidhankadu@gmail.com>
2026-03-06 03:25:28 +0530

    feat(ui): implement fully responsive layout across Wanderlust app (listings, filters, forms, map)

--- controllers/listings.js
+++ controllers/listings.js
@@ -49,7 +49,7 @@ module.exports.createListing = async (req, res, next) => {
         },
 
         headers: {
-          "User-Agent": "Wanderlust-vidhan",
+          "User-Agent": "vidhan-kadu",
         },
       },
     );
